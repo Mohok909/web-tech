795666a03d4a0bf851d59db002de7d624955e964
Mohok909 <88015407+Mohok909@users.noreply.github.com>
2021-11-21 13:45:09 +0600

    Add files via upload

---
+++ Calculator/index.js
@@ -0,0 +1,33 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error";
+                     window.setInterval('refresh()', 10000);    
+    
+                    function refresh() {
+                    window .location.reload();
+                }
+
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
